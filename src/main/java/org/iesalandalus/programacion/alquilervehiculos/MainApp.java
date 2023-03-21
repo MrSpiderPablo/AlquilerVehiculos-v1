4b40926783e124ad27b4443858968a696f286285
pablo <pabloramosblanquez@gmail.com>
2023-03-21 14:18:40 +0100

    Commit final

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -1,22 +1,31 @@
 package org.iesalandalus.programacion.alquilervehiculos;
 
 import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.FactoriaFuenteDatos;
 import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.ModeloCascada;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.negocio.IFuenteDatos;
+import org.iesalandalus.programacion.alquilervehiculos.vista.FactoriaVista;
 //import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
 import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
 import org.iesalandalus.programacion.alquilervehiculos.vista.texto.Consola;
+import org.iesalandalus.programacion.alquilervehiculos.vista.texto.VistaTexto;
 
 public class MainApp {
 
 	public static void main(String[] args) 
 	{
 		// Ánimo!!!!
-//		Modelo modelo = new Modelo();
-//		Vista vista = new Vista();
-//		Controlador controlador = new Controlador(modelo, vista);
-//		controlador.comenzar();
+		FactoriaFuenteDatos.MEMORIA.crear();
+		FactoriaVista.TEXTO.crear();
+		IFuenteDatos fuenteDatos = null;
+		ModeloCascada modelo = new ModeloCascada(fuenteDatos);
+		VistaTexto vista = new VistaTexto();
+		Controlador controlador = new Controlador(modelo, vista);
+		controlador.comenzar();
+		Consola.elegirAccion();
 		
-		Consola.mostrarMenuAcciones();
+//		Consola.mostrarMenuAcciones();
 	}
 
 }
